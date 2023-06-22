679778f6fb0dd60f5e586867cd323cb6c9e038ee
Batuhan Kazan <batuhankzn61@gmail.com>
2023-06-22 19:02:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import requests
+import datetime
 
 screen = Tk()
 screen.title("Crypto Dashboard")
@@ -10,8 +11,6 @@ FONT = ("Verdana",20,"normal")
 def get_crypto_data():
     response = requests.get(url="https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")
     return response.json()
-
-
 
 
 search_title = Label(text="Search Crypto: ")
@@ -26,9 +25,9 @@ def button_clicked():
     crypto_response = get_crypto_data()
     for crypto in crypto_response:
         if crypto["currency"] == search_crypto:
-            print(crypto["currency"],": ",crypto["price"])
+            label_text = f"{crypto['currency']}: {crypto['price']}"
+            crypto_message.config(text=label_text)
             break
-
 
 
 
@@ -36,13 +35,18 @@ search_button = Button(text="Search",command=button_clicked)
 search_button.place(x=175,y=150)
 
 
+
 crypto_message = Label()
 crypto_message.place(x=175,y=50)
 
+def update_clock():
+    current_time = datetime.datetime.now().strftime('%H:%M:%S')
+    timeLabel.config(text=current_time)
+    timeLabel.after(1000, update_clock)
+
+timeLabel = Label()
+timeLabel.place(x=300,y=400)
 
 
-
-
-
-
+update_clock()
 screen.mainloop()
